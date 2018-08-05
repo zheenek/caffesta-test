c17a5cb54ef5ff66c38bf791edff74ef3f5f91dc
zheenek <zheenek2000@gmail.com>
2018-08-05 15:21:24 +0300

    guess this end

--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ import Page from './components/Page';
 //const baseURL = process.env.PUBLIC_URL;
 
 class App extends Component {
+
+  state = {
+    letter: 'A',
+  }
+
+  renderA = () => this.state.letter==="B" ? this.setState({letter: "A"}) : this.setState({});
+  renderB = () => this.state.letter==="A" ? this.setState({letter: "B"}) : this.setState({});
+
   render() {
     return (
       <Router>
         <Switch>
-            <Route exact path="/" component={Application}/>
+            <Route exact path="/" render={() => (<Application 
+              letter={this.state.letter} 
+              switchToA={this.renderA}
+              switchToB={this.renderB}
+              />)}/>
             <Route path="/a" render={() =>(<Page letter="A"/>)} />
             <Route path="/b" render={() =>(<Page letter="B"/>)} />
         </Switch>
--- src/components/App.js
+++ src/components/App.js
@@ -17,72 +17,36 @@ const modalStyle = {
 
 class Application extends Component {
     state = {
-        letter: "A",
-        isActive: true,
         firstModalOpen: false,
         secondModalOpen: false
     }
 
-    setA = () => {
-        this.setState({
-            letter: "A"
-        });
-        console.log(this.state.letter);
-    }
-
-    setB = () => {
-        this.setState({
-            letter: "B"
-        });
-        console.log(this.state.letter);
-    }
-
-    openModal = () => {
-        this.setState({
-            firstModalOpen: true
-        });
-    }
-
-    openSecondModal = () => {
-        this.setState({
-            secondModalOpen: true
-        });
-    }
-
-    closeModal = () => {
-        const cancelOne = document.getElementById("closeFirstModal");
-        const cancelTwo = document.getElementById("closeSecondModal");
-
-        if(cancelTwo) {
-            this.setState({
-                secondModalOpen: false
-            });
-        } else if (cancelOne) {
-            this.setState({
-                firstModalOpen: false
-            });
-        }
-    }
+    openModal = () => this.setState({firstModalOpen: true});
+    openSecondModal = () => this.setState({secondModalOpen: true});
+    
+    closeFirstModal = () => this.setState({firstModalOpen: false});
+    closeSecondModal = () => this.setState({secondModalOpen: false});
 
     render() {
+        const active = this.props.letter;
         return (
             <div className="container">
-                <Label currentValue={this.state.letter}/>
-                <NavLink exact to="/a"><Button id="btnA" onClick={this.setA} text="A"/></NavLink>
-                <Button id="btnB" onClick={this.openModal} text="B"/>
+                <Label currentValue={this.props.letter}/>
+                <NavLink exact to="/a"><Button className={active==="A" ? "active" : ''} onClick={this.props.switchToA} text="A"/></NavLink>
+                <Button id="btnB" className={active==="B" ? "active" : ''} onClick={this.openModal} text="B"/>
 
                 {/*FIRST MODAL*/}
                 <Modal isOpen={this.state.firstModalOpen} style={modalStyle} ariaHideApp={false}>
                     <h2>First Modal</h2>
                     <Button onClick={this.openSecondModal} text="Ok" />
-                    <Button id="closeFirstModal" onClick={this.closeModal} text="Cancel"/>
+                    <Button onClick={this.closeFirstModal} text="Cancel"/>
                 </Modal>
 
                 {/*SECOND MODAL*/}
                 <Modal isOpen={this.state.secondModalOpen} style={modalStyle} ariaHideApp={false}>
                     <h2>Second Modal</h2>
-                    <NavLink to="/b"><Button id="openB" onClick={this.setB} text="Ok" /></NavLink>
-                    <Button id="closeSecondModal" onClick={this.closeModal} text="Cancel" />
+                    <NavLink to="/b"><Button onClick={this.props.switchToB} text="Ok" /></NavLink>
+                    <Button onClick={this.closeSecondModal} text="Cancel" />
                 </Modal>
             </div>
         );
